bf582580e5426b5239659d5ccae312413c2bd1b7
Akhu117 <anthony@DESKTOP-DG0VB10.localdomain>
2018-11-04 00:34:07 +0100

    Constraint Layout : Ok

--- app/src/main/java/com/example/antho/androidcourse/MainActivity.java
+++ app/src/main/java/com/example/antho/androidcourse/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.antho.androidcourse;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count); //Binding view and variable
     }
 
     public void showToast(View view) {
@@ -19,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    @SuppressLint("SetTextI18n")
     public void countUp(View view) {
+        mCount++;
+
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
