e6383954c5fdc2cba205b2e9f618801888c41b38
adacruz <anthony.dacruz@aboutgoods.net>
2018-11-05 17:02:47 +0100

    data activity

---
+++ app/src/main/java/com/example/antho/androidcourse/DataPassActivity.java
@@ -0,0 +1,19 @@
+package com.example.antho.androidcourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DataPassActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_pass);
+    }
+
+    public void launchSecondActivity(View view) {
+    }
+}
